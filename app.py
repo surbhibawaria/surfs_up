f2965a18d715bd5e9817831622433f432f5bce85
Surbhi Bawaria <surbhibawaria@Surbhis-MBP.attlocal.net>
2022-02-24 21:14:03 -0500

    adding starter code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-#from flask import Flask
-#app = Flask(__name__)
-#@app.route('/')
-#def hello_world():
-    #return 'Hello world'
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello world'
 
 import datetime as dt
 import numpy as np
